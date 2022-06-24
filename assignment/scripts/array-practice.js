82e680f7af655cd793342390a2eef19597d0654f
Taylor Moe <taylormoe93@gmail.com>
2022-06-24 13:24:51 -0500

    Completed all required questions

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -34,6 +34,7 @@ console.log('Second animal is', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index
 console.log('Last animal is', animalArray[3]);
+
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
 console.log(animalArray[animalArray.length - 1]);
@@ -46,6 +47,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
+foodsArray.push('soup');
+console.log('Added a food to the end,', foodsArray);
 
 // Example: Remove the last animal by using Array.pop
 let removedAnimal = animalArray.pop();
@@ -54,12 +57,17 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array &
 //      log both the food removed and the updated array
+let removedFood = foodsArray.pop();
+console.log('Removed the last food,', removedFood);
+console.log('The foods are now,', foodsArray);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
+foodsArray.unshift('moreSoup');
+console.log('Added food to the beginning:', foodsArray);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -68,6 +76,9 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array &
 //     log both the food removed and the updated array
+removedFood = foodsArray.shift();
+console.log('Removed the first food,', removedFood);
+console.log('The foods are now', foodsArray);
 
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
